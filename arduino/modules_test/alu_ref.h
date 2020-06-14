9bd3d123322b52e30b8287f61e3762b29ae40102
Jurģis Brigmanis <velko.x@gmail.com>
2020-06-14 21:17:59 +0300

    flags_of_add8/sub8_with_c

--- arduino/modules_test/alu_ref.h
+++ arduino/modules_test/alu_ref.h
@@ -10,6 +10,9 @@ uint8_t flags_of_sub8(uint8_t a, uint8_t b);
 uint8_t add8_set_carry(uint8_t a, uint8_t b);
 uint8_t sub8_set_carry(uint8_t a, uint8_t b);
 
+uint8_t flags_of_add8_with_c(uint8_t a, uint8_t b);
+uint8_t flags_of_sub8_with_c(uint8_t a, uint8_t b);
+
 
 #ifdef __cplusplus
 }
