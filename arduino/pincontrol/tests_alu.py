3ea3be18db3664681066dcd95b5c89523ba04203
Jurģis Brigmanis <velko.x@gmail.com>
2021-01-15 00:35:18 +0200

    Refactor register tests

--- arduino/pincontrol/tests_alu.py
+++ arduino/pincontrol/tests_alu.py
@@ -11,80 +11,59 @@ ser = serial.Serial("/dev/ttyACM0", 9600, timeout=3)
 pins = PinClient(ser)
 cpu = CPU(pins)
 
+# Common test cases for registers
+# see RegisterALoadOut, RegisterBLoadOut
+# for actual usage
+class RegisterLoadOutCommon:
+    def load_test_value(self, value):
 
-class RegisterLoadOut(unittest.TestCase):
-
-    def load_test_value(self, register, value):
-
-        ldi (register, value)
-        received = out(register)
+        ldi (self.register, value)
+        received = out(self.register)
 
         self.assertEqual(value, received)
 
-    # Register A
-    def test_load_A_0(self):
-        self.load_test_value(A, 0)
+    def test_load_0(self):
+        self.load_test_value(0)
 
-    def test_load_A_1(self):
-        self.load_test_value(A, 1)
+    # Check each bit separately
+    def test_load_1(self):
+        self.load_test_value(1)
 
-    def test_load_A_2(self):
-        self.load_test_value(A, 2)
+    def test_load_2(self):
+        self.load_test_value(2)
 
-    def test_load_A_4(self):
-        self.load_test_value(A, 4)
+    def test_load_4(self):
+        self.load_test_value(4)
 
-    def test_load_A_8(self):
-        self.load_test_value(A, 8)
+    def test_load_8(self):
+        self.load_test_value(8)
 
-    def test_load_A_16(self):
-        self.load_test_value(A, 16)
+    def test_load_16(self):
+        self.load_test_value(16)
 
-    def test_load_A_32(self):
-        self.load_test_value(A, 32)
+    def test_load_32(self):
+        self.load_test_value(32)
 
-    def test_load_A_64(self):
-        self.load_test_value(A, 64)
+    def test_load_64(self):
+        self.load_test_value(64)
 
-    def test_load_A_128(self):
-        self.load_test_value(A, 128)
+    def test_load_128(self):
+        self.load_test_value(128)
 
-    def test_load_A_255(self):
-        self.load_test_value(A, 255)
+    def test_load_255(self):
+        self.load_test_value(255)
 
 
-    # Register B
-    def test_load_B_0(self):
-        self.load_test_value(B, 0)
+class RegisterALoadOut(unittest.TestCase, RegisterLoadOutCommon):
+    def setUp(self):
+        self.register = A
 
-    def test_load_B_1(self):
-        self.load_test_value(B, 1)
+class RegisterBLoadOut(unittest.TestCase, RegisterLoadOutCommon):
+    def setUp(self):
+        self.register = B
 
-    def test_load_B_2(self):
-        self.load_test_value(B, 2)
 
-    def test_load_B_4(self):
-        self.load_test_value(B, 4)
-
-    def test_load_B_8(self):
-        self.load_test_value(B, 8)
-
-    def test_load_B_16(self):
-        self.load_test_value(B, 16)
-
-    def test_load_B_32(self):
-        self.load_test_value(B, 32)
-
-    def test_load_B_64(self):
-        self.load_test_value(B, 64)
-
-    def test_load_B_128(self):
-        self.load_test_value(B, 128)
-
-    def test_load_B_255(self):
-        self.load_test_value(B, 255)
-
-    #ALU operations
+class AluOperations(unittest.TestCase):
     def test_add_ab_result_small(self):
         ldi(A, 24)
         ldi(B, 18)
