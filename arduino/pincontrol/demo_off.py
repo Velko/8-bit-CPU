bf89ca953fdbc9b47f6aa9b1e342fe8254bf44e5
Jurģis Brigmanis <velko.x@gmail.com>
2021-01-31 21:21:43 +0200

    Fix demo_off

--- arduino/pincontrol/demo_off.py
+++ arduino/pincontrol/demo_off.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from libpins.cpu import *
+
 def run():
 
     # "Low power mode" - turn off as most LEDs as
@@ -16,5 +18,5 @@ if __name__ == "__main__":
 
     from libpins import PyAsmExec
 
-    PyAsmExec.setup(globals())
+    PyAsmExec.setup()
     run()
