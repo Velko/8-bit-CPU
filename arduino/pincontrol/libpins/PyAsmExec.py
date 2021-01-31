92bb7e21d20fbd10bb88bc27096e45165602db4e
Jurģis Brigmanis <velko.x@gmail.com>
2021-01-31 20:14:32 +0200

    Autodetect the serial port

--- arduino/pincontrol/libpins/PyAsmExec.py
+++ arduino/pincontrol/libpins/PyAsmExec.py
@@ -1,12 +1,23 @@
 
-import serial
+import serial, os
 
 from . import asm
 from .PinClient import PinClient
 from .cpu import CPU
 from .ctrl_word import CtrlWord
 
-ser = serial.Serial("/dev/ttyACM0", 115200, timeout=3)
+def find_serial_port():
+    ports = list(filter(lambda fn: fn.startswith("ttyACM") or fn.startswith("ttyUSB"),  os.listdir("/dev")))
+
+    if len(ports) > 1:
+        raise Exception("Multiple USB serial devices found")
+
+    if len(ports) == 0:
+        raise Exception("No USB serial devices found")
+
+    return os.path.join("/dev", ports[0])
+
+ser = serial.Serial(find_serial_port(), 115200, timeout=3)
 pins = PinClient(ser)
 cpu = CPU(pins, CtrlWord())
 
