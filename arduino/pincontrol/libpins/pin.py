4e38a78f2ff9e0db1f7d680a9f9300cc964912b9
Jurģis Brigmanis <velko.x@gmail.com>
2021-01-31 17:57:44 +0200

    Add Mux and MuxPin

--- arduino/pincontrol/libpins/pin.py
+++ arduino/pincontrol/libpins/pin.py
@@ -38,3 +38,42 @@ class NullPin:
 
     def disable(self):
         pass
+
+class Mux:
+    def __init__(self, pins, default):
+        self.control_word = None
+        self.pins = pins
+        self.default = default
+
+    def connect(self, control_word):
+        self.control_word = control_word
+
+    def enable(self, num):
+        for bit_idx, pin in enumerate(self.pins):
+            if (num & (1 << bit_idx)) != 0:
+                self.control_word.set(pin)
+            else:
+                self.control_word.clr(pin)
+
+    def disable(self):
+        self.enable(self.default)
+
+class MuxPin:
+    def __init__(self, mux, num):
+        self.mux = mux
+        self.num = num
+
+    # There are some inefficiencies how MuxPin handles passing
+    # the data back to Mux, but it does not matter much because
+    # it occurs purely in Python
+    # for example: Mux.connect() is called for by each MuxPin's
+    # connect(), while calling it only once per Mux should be
+    # enough. But it does not make any noticable impact on peformance
+    def connect(self, control_word):
+        self.mux.connect(control_word)
+
+    def enable(self):
+        self.mux.enable(self.num)
+
+    def disable(self):
+        self.mux.disable()
