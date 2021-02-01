52961151e808709efcfc3aad65913aaf73099762
Jurģis Brigmanis <velko.x@gmail.com>
2021-02-01 21:47:59 +0200

    Rewrite high-level RAM tests to check more addresses

--- pycontrol/tests/test_ram.py
+++ pycontrol/tests/test_ram.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
 
-import pytest
+import pytest, random
 
 from libcpu.cpu import *
 
-def test_store_load(cpu_backend_real):
-    ldi (B, 42)
-    ldi (A, 11)
 
-    st (5, B)
-    ld (A, 5)
+# can not make as a fixture, because it can not be
+# unpacked for parametrization (couldn't find a way)
+def make_random_addr():
 
-    val = peek (A)
+    # make sure addresses are unique
+    addr = list(range(256))
+    random.shuffle(addr)
 
-    assert val == 42
+    # 32 addresses out of 256 should be enough to
+    # verify that RAM works as expected
+    return addr[:32]
+
+random_addr = make_random_addr()
+
+
+@pytest.fixture(scope="module")
+def fill_ram(random_bytes, cpu_backend_real):
+    for addr in random_addr:
+        ldi (A, random_bytes[addr])
+        st (addr, A)
+
+
+@pytest.mark.parametrize("addr", random_addr)
+def test_store_load(cpu_backend_real, random_bytes, fill_ram, addr):
+
+    ld (A, addr)
+    actual = peek(A)
+
+    assert random_bytes[addr] == actual
