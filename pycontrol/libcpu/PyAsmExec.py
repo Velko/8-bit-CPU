dd1e763f9be3e833a33900add1f82930e7c3f481
Jurģis Brigmanis <velko.x@gmail.com>
2021-02-03 21:29:55 +0200

    Connect to hardware only when about to execute

--- pycontrol/libcpu/PyAsmExec.py
+++ pycontrol/libcpu/PyAsmExec.py
@@ -4,9 +4,9 @@ from . import cpu
 from .ctrl_word import CtrlWord
 from .cpu_exec import CPUBackendControl
 
-pins = PinClient()
 control = CtrlWord()
 
 def setup():
+    pins = PinClient()
     backend = CPUBackendControl(pins, control)
     cpu.install_cpu_backend(backend)
