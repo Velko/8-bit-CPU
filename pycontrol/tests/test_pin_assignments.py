3bc4e16f1e7c37168695c600299633945137373c
Jurģis Brigmanis <velko.x@gmail.com>
2021-02-03 23:32:07 +0200

    Test mux pins against eachother

--- pycontrol/tests/test_pin_assignments.py
+++ pycontrol/tests/test_pin_assignments.py
@@ -28,3 +28,30 @@ def each_simple_pin_and_addr_with_others():
 def test_simple_pin_and_mux_addr_overlap(name_a, pin_a, name_b, pin_b):
 
     assert pin_a != pin_b
+
+
+def pins_in_mux(mux):
+    for name, pin in all_pins():
+        if isinstance(pin, MuxPin):
+            if pin.mux == mux:
+                yield name, pin.num
+
+    # also add default as assigned pin
+    yield "default", mux.default
+
+def each_pin_with_others_in_mux():
+
+   for mux_name, mux in all_muxes():
+        mpins = list(pins_in_mux(mux))
+
+        for a_id, (a_name, a_num) in enumerate(mpins):
+            for b_name, b_num in mpins[a_id+1:]:
+                yield "{}.{}".format(mux_name, a_name), a_num, "{}.{}".format(mux_name, b_name), b_num
+
+@pytest.mark.parametrize("name_a,pin_a,name_b,pin_b", each_pin_with_others_in_mux())
+def test_mux_pin_overlap(name_a, pin_a, name_b, pin_b):
+
+    if name_a == name_b:
+        pytest.skip("comparing aliased pins")
+
+    assert pin_a != pin_b
