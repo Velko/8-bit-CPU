a7188f63d0dba85779820185c8bad022d65832b8
Jurģis Brigmanis <velko.x@gmail.com>
2021-02-07 22:15:06 +0200

    Easy-to-exclude bootloader

--- pycontrol/demo/prime_sieve.py
+++ pycontrol/demo/prime_sieve.py
@@ -7,33 +7,35 @@ from libcpu.cpu import *
 sieve_start = Label()
 
 def run():
+
     # bootloader
+    if True:       #edit True/False to include/exclude
 
-    # temporary hardware configuration is such that reads
-    # comes from ROM, but writes goes into RAM
-    # bootloader loops all 256 addresses, reads in and writes
-    # back (making a copy from ROM to RAM in the process)
+        # temporary hardware configuration is such that reads
+        # comes from ROM, but writes goes into RAM
+        # bootloader loops all 256 addresses, reads in and writes
+        # back (making a copy from ROM to RAM in the process)
 
-    # dummy instruction, will be replaced by jmp when loading
-    # is complete
-    ldi (A, sieve_start)
+        # dummy instruction, will be replaced by jmp when loading
+        # is complete
+        ldi (A, sieve_start)
 
-    ldi (B, 0)
-    while True:
-        ldabs (A, B)
+        ldi (B, 0)
+        while True:
+            ldabs (A, B)
+            stabs (B, A)
+            ldi (A, 1)
+            add (B, A)
+            if bcs(): break
+
+        # after address wrap-around B should be 0, the address we need;
+        # replace the value there with opcode for jmp (be careful with microcode updates)
+        ldi (A, 0x24)
         stabs (B, A)
-        ldi (A, 1)
-        add (B, A)
-        if bcs(): break
 
-    # after address wrap-around B should be 0, the address we need
-    # replace the value there with opcode for jmp (be careful with microcode updates)
-    ldi (A, 0x24)
-    stabs (B, A)
-
-    # run halt in a loop in case it is not wired properly
-    while True:
-        hlt()
+        # run halt in a loop in case it is not wired properly
+        while True:
+            hlt()
 
     # now the program
     sieve_start.here()
