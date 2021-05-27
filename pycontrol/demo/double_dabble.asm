d87a19b15684ce1a4553c477c3dbf2848f5937e2
Jurģis Brigmanis <velko.x@gmail.com>
2021-05-27 21:56:45 +0300

    Use calculated constants for buffer sizes/indexes

--- pycontrol/demo/double_dabble.asm
+++ pycontrol/demo/double_dabble.asm
@@ -10,7 +10,7 @@ start:
 
 ; ---------------------------------------------------------------------------------------
 convert_number:
-    ldi (D, 32)  ; repeat for each bit
+    ldi (D, number_len * 8)  ; repeat for each bit
     ldi (B, 5)   ; for comparison
 
 double_loop:
@@ -21,7 +21,7 @@ double_loop:
     clr (A)
     push (A)
 
-    ldi (C, 3)  ; index in number[]
+    ldi (C, number_len - 1)  ; index in number[]
 num_shift_loop:
     ldx (A, number, C)
     popf () ; load flags (initial C = 0, or one from previous iteration)
@@ -36,7 +36,7 @@ num_shift_loop:
 
     ; now number[] is shifted left one place, flags with C bit on stack
 
-    ldi (C, 9)  ; index in digits[]
+    ldi (C, digits_len - 1)  ; index in digits[]
 add3_shift_loop:
 
     ; check each digit if >=5
@@ -70,7 +70,7 @@ add3_skip:
 
     ; convert contents of digits to ASCTI characters
     ldi (B, 0x30) ; code of '0'
-    ldi (C, 9)
+    ldi (C, digits_len - 1)
 
 to_char_loop:
 
@@ -87,7 +87,7 @@ to_char_loop:
 print_digits:
     ; find first non-0 digit
     ldi (B, 0x30) ; code of '0'
-    ldi (D, 9)  ; upper limit for search
+    ldi (D, digits_len - 1)  ; upper limit for search (keep last digit)
     clr (C)
 find0_loop:
     ldx (A, digits, C)
@@ -115,8 +115,11 @@ print_end:
 ; as \0 separator after conversion is complete
 digits:
     #d8 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+digits_len = $ - digits
+
 number:
     #d32 36324058  ; Big endian format
+number_len = $ - number
 
 stack_end:
     #res 16
