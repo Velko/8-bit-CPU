fea39fd6d57df53f9ace4979b708b66c840c6722
Jurģis Brigmanis <velko.x@gmail.com>
2021-06-15 22:22:53 +0300

    Show gaps in pinmap

--- pycontrol/tools/pinmap.py
+++ pycontrol/tools/pinmap.py
@@ -4,12 +4,36 @@ import localpath
 
 from libcpu.discovery import simple_pins, mux_pins, all_muxes
 from libcpu.pin import Pin, MuxPin
-from typing import Sequence, Tuple
+from typing import Dict, Sequence, Tuple
 
-for name, pin in sorted(simple_pins(), key=lambda pin: pin[1].num):
-    print ("{:2} {}".format(pin.num, name))
+pinmap: Dict[int, str] = {}
+
+for name, pin in simple_pins():
+    pinmap[pin.num] = name
+
+for name, mux in all_muxes():
+    for i, mp in enumerate(mux.pins):
+        pinmap[mp] = f"{name}.{i}"
+
+for p in range(32):
+    if p in pinmap:
+        print (f"{p:2} {pinmap[p]}")
+    else:
+        print (f"{p:2} -")
 
 for name, mux in all_muxes():
     print (name, mux.pins)
-    for name, mpin in sorted(mux_pins(mux), key=lambda pin: pin[1].num):
-        print ("  ", mpin.num, name)
+    mpm: Dict[int, str] = {}
+
+    for mpname, mpin in mux_pins(mux):
+        mpm[mpin.num] = mpname
+
+    mpm[mux.default] = "(default)"
+
+    capacity = 2**len(mux.pins)
+
+    for p in range(capacity):
+        if p in mpm:
+            print (f"  {p:2} {mpm[p]}")
+        else:
+            print (f"  {p:2} -")
