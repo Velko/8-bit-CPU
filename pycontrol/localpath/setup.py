19f85fab615515a1ec8f9dabe8030100b0c14e4f
Jurģis Brigmanis <velko.x@gmail.com>
2021-06-26 21:38:47 +0300

    Setup script for localpath

---
+++ pycontrol/localpath/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='Localpath',
+      version='0.1',
+      description='Add local packages to path',
+      author='Jurģis Brigmanis',
+      author_email='velko.x@gmail.com',
+      packages=['localpath'],
+      package_dir={'localpath': '.'}
+     )
