6d1b910ff53e3e0cbc582ff230b4932439ee7d0a
Jurģis Brigmanis <velko.x@gmail.com>
2021-07-08 19:06:59 +0300

    Option to add test args for emu_check

--- pycontrol/emu_check.sh
+++ pycontrol/emu_check.sh
@@ -4,6 +4,6 @@ socat PTY,link=pty0,raw,echo=0 PTY,link=pty1,raw,echo=0 &
 sleep 0.1
 ../arduino/emulpins/emulpins &
 sleep 0.1
-SER_PORT=pty0 python3 -m pytest
+SER_PORT=pty0 python3 -m pytest $@
 
 kill -- -$$
