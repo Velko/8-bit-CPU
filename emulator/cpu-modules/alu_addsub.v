5da385c20fdd856374925ef0560e7c21bcc2851e
Jurģis Brigmanis <velko.x@gmail.com>
2021-10-16 14:41:02 +0300

    Switch ALU to pins variant of chips where appropiate

--- emulator/cpu-modules/alu_addsub.v
+++ emulator/cpu-modules/alu_addsub.v
@@ -8,18 +8,15 @@ module alu_addsub (
     output cout,
     output vout);
 
-    xor_86b xor_flags(.a({xor_h.y[3], arg_l[7], cin, add_h.cout}), .b({add_h.s[3], add_h.s[3], sub, sub}));
-    and_08b and_v(.a({3'bx, xor_flags.y[2]}), .b({3'bx, xor_flags.y[3]}));
+    xor_86p xor_flags(.a4(xor_h.y[3]), .a3(arg_l[7]), .a2(cin), .a1(add_h.cout), .b4(add_h.s[3]), .b3(add_h.s[3]), .b2(sub), .b1(sub), .y1(cout));
+    and_08p and_v(.a1(xor_flags.y3), .b1(xor_flags.y4), .y1(vout));
 
     xor_86b xor_l(.a(arg_r[3:0]), .b({sub, sub, sub, sub}));
-    adder_283 add_l(.a(arg_l[3:0]), .b(xor_l.y), .cin(xor_flags.y[1]));
+    adder_283 add_l(.a(arg_l[3:0]), .b(xor_l.y), .cin(xor_flags.y2));
 
     xor_86b xor_h(.a(arg_r[7:4]), .b({sub, sub, sub, sub}));
     adder_283 add_h(.a(arg_l[7:4]), .b(xor_h.y), .cin(add_l.cout));
 
     buffer_245 bus_buf(.oen(outn), .dir(1'b1), .a({add_h.s, add_l.s}), .b(bus));
-
-    assign cout = xor_flags.y[0];
-    assign vout = and_v.y[0];
 
 endmodule
