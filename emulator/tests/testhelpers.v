e80812fe2977c5efdf47d6f3d4df13485ae1aec0
Jurģis Brigmanis <velko.x@gmail.com>
2021-10-17 12:27:51 +0300

    Convert test macros to one-liners

--- emulator/tests/testhelpers.v
+++ emulator/tests/testhelpers.v
@@ -1,11 +1,2 @@
-`define assert(signal, value) \
-    if (signal !== value) begin \
-        $display("ASSERTION FAILED in %m: signal != value, was %b", signal); \
-        $fatal; \
-    end
-
-`define tick(signal, times ) \
-    repeat (times) begin \
-        signal <= ~signal; \
-        #5; \
-    end
+`define assert(signal, value) if (signal !== value) begin $display("ASSERTION FAILED in %m: signal != value, was %b", signal); $fatal; end
+`define tick(signal, times ) repeat (times) begin signal <= ~signal; #5; end
