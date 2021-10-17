3d9c945c760b3c86c7aa3ac0cc3d4a25aa816bb2
Jurģis Brigmanis <velko.x@gmail.com>
2021-10-17 15:25:25 +0300

    Output flags result

--- emulator/cpu-modules/alu_block.v
+++ emulator/cpu-modules/alu_block.v
@@ -1,5 +1,6 @@
 module alu_block(
         inout [7:0] main_bus,
+        output [3:0] fout,
 
         input reset,
         input a_outn,
@@ -26,6 +27,6 @@ module alu_block(
     gp_register b(.bus(main_bus), .alu_l(alu_arg_l), .alu_r(alu_arg_r), .reset(reset), .clk(clk), .iclk(iclk), .outn(b_outn), .loadn(b_loadn), .loutn(1'b1), .routn(1'b0));
 
     alu_addsub addsub(.bus(main_bus), .arg_l(alu_arg_l), .arg_r(alu_arg_r), .outn(addsub_outn), .sub(alt), .cin(1'b0), .vout(vfb), .cout(cfb));
-    flags_reg flags(.bus(main_bus), .reset(reset), .clk(clk), .iclk(iclk), .boutn(1'b1), .bloadn(1'b1), .vin(vfb), .cin(cfb), .calcn(calcfn));
+    flags_reg flags(.bus(main_bus), .reset(reset), .clk(clk), .iclk(iclk), .boutn(1'b1), .bloadn(1'b1), .vin(vfb), .cin(cfb), .calcn(calcfn), .fout(fout));
 
 endmodule
