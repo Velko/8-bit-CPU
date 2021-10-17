0c8113e12174f1329c2e04cf5285e02c2d351fe1
Jurģis Brigmanis <velko.x@gmail.com>
2021-10-17 14:12:33 +0300

    Simplify for Verilator

--- emulator/cpu-modules/alu_addsub.v
+++ emulator/cpu-modules/alu_addsub.v
@@ -8,16 +8,18 @@ module alu_addsub (
     output cout,
     output vout);
 
+    wire [7:0] out_v;
+
     xor_86p xor_flags(.a4(xor_h.y[3]), .a3(arg_l[7]), .a2(cin), .a1(add_h.cout), .b4(add_h.s[3]), .b3(add_h.s[3]), .b2(sub), .b1(sub), .y1(cout));
     and_08p and_v(.a1(xor_flags.y3), .b1(xor_flags.y4), .y1(vout),
         .a2(1'b0), .a3(1'b0), .a4(1'b0), .b2(1'b0), .b3(1'b0), .b4(1'b0));
 
     xor_86b xor_l(.a(arg_r[3:0]), .b({sub, sub, sub, sub}));
-    adder_283 add_l(.a(arg_l[3:0]), .b(xor_l.y), .cin(xor_flags.y2));
+    adder_283 add_l(.a(arg_l[3:0]), .b(xor_l.y), .cin(xor_flags.y2), .s(out_v[3:0]));
 
     xor_86b xor_h(.a(arg_r[7:4]), .b({sub, sub, sub, sub}));
-    adder_283 add_h(.a(arg_l[7:4]), .b(xor_h.y), .cin(add_l.cout));
+    adder_283 add_h(.a(arg_l[7:4]), .b(xor_h.y), .cin(add_l.cout), .s(out_v[7:4]));
 
-    buffer_245 bus_buf(.oen(outn), .dir(1'b1), .a({add_h.s, add_l.s}), .b(bus));
+    buffer_245 bus_buf(.oen(outn), .dir(1'b1), .a(out_v), .b(bus));
 
 endmodule
--- emulator/cpu-modules/gp_register.v
+++ emulator/cpu-modules/gp_register.v
@@ -12,14 +12,17 @@ module gp_register (
     output [7:0] alu_l,
     output [7:0] alu_r);
 
-    dff_173 prim_l(.mr(reset), .cp(clk), .e1n(loadn), .e2n(loadn), .oe1n(1'b0), .oe2n(1'b0), .d(bus[3:0]));
-    dff_173 prim_h(.mr(reset), .cp(clk), .e1n(loadn), .e2n(loadn), .oe1n(1'b0), .oe2n(1'b0), .d(bus[7:4]));
-    buffer_245 bus_buf(.oen(outn), .dir(1'b1), .a({prim_h.q, prim_l.q}), .b(bus));
+    wire [7:0] out_v;
+    wire [7:0] alu_v;
 
-    dff_173 sec_l(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(1'b0), .oe2n(1'b0), .d(prim_l.q));
-    dff_173 sec_h(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(1'b0), .oe2n(1'b0), .d(prim_h.q));
+    dff_173 prim_l(.mr(reset), .cp(clk), .e1n(loadn), .e2n(loadn), .oe1n(1'b0), .oe2n(1'b0), .d(bus[3:0]), .q(out_v[3:0]));
+    dff_173 prim_h(.mr(reset), .cp(clk), .e1n(loadn), .e2n(loadn), .oe1n(1'b0), .oe2n(1'b0), .d(bus[7:4]), .q(out_v[7:4]));
+    buffer_245 bus_buf(.oen(outn), .dir(1'b1), .a(out_v), .b(bus));
 
-    buffer_245 alu_l_buf(.oen(loutn), .dir(1'b1), .a({sec_h.q, sec_l.q}), .b(alu_l));
-    buffer_245 alu_r_buf(.oen(routn), .dir(1'b1), .a({sec_h.q, sec_l.q}), .b(alu_r));
+    dff_173 sec_l(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(1'b0), .oe2n(1'b0), .d(prim_l.q), .q(alu_v[3:0]));
+    dff_173 sec_h(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(1'b0), .oe2n(1'b0), .d(prim_h.q), .q(alu_v[7:4]));
+
+    buffer_245 alu_l_buf(.oen(loutn), .dir(1'b1), .a(alu_v), .b(alu_l));
+    buffer_245 alu_r_buf(.oen(routn), .dir(1'b1), .a(alu_v), .b(alu_r));
 
 endmodule
