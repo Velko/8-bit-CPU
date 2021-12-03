1c4e680cb9b12d6b27683ad4172f6d8e771bf361
Jurģis Brigmanis <velko.x@gmail.com>
2021-12-03 21:50:49 +0200

    PC tests and fixes

--- emulator/cpu-modules/program_counter.v
+++ emulator/cpu-modules/program_counter.v
@@ -16,10 +16,10 @@ module program_counter (
     counter_161 cnt_2(.clk(clk), .mrn(resetn), .pen(loadn), .cep(count), .d(abus[11:8]), .cet(cnt_1.tc));
     counter_161 cnt_3(.clk(clk), .mrn(resetn), .pen(loadn), .cep(count), .d(abus[15:12]), .cet(cnt_2.tc));
 
-    dff_173 outst_0(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(1'b0), .oe2n(1'b0), .d(cnt_0.q), .q(abus[3:0]));
-    dff_173 outst_1(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(1'b0), .oe2n(1'b0), .d(cnt_1.q), .q(abus[7:4]));
-    dff_173 outst_2(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(1'b0), .oe2n(1'b0), .d(cnt_2.q), .q(abus[11:8]));
-    dff_173 outst_3(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(1'b0), .oe2n(1'b0), .d(cnt_3.q), .q(abus[15:12]));
+    dff_173 outst_0(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(outn), .oe2n(outn), .d(cnt_0.q), .q(abus[3:0]));
+    dff_173 outst_1(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(outn), .oe2n(outn), .d(cnt_1.q), .q(abus[7:4]));
+    dff_173 outst_2(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(outn), .oe2n(outn), .d(cnt_2.q), .q(abus[11:8]));
+    dff_173 outst_3(.mr(reset), .cp(iclk), .e1n(1'b0), .e2n(1'b0), .oe1n(outn), .oe2n(outn), .d(cnt_3.q), .q(abus[15:12]));
 
 
 endmodule
--- emulator/tests/tb_program_counter.v
+++ emulator/tests/tb_program_counter.v
@@ -10,10 +10,12 @@ module tb_program_counter;
     wire resetn;
 
     wire [15:0] abus;
-    reg [7:0] addr;
+    reg [15:0] addr;
     reg wbus;
 
-    program_counter pc (.outn(outn), .loadn(loadn), .count(count), .clk(clk), .iclk(iclk), .reset(reset), .resetn(resetn));
+    integer i;
+
+    program_counter pc (.outn(outn), .loadn(loadn), .count(count), .clk(clk), .iclk(iclk), .reset(reset), .resetn(resetn), .abus(abus));
 
 initial begin
         $display("Program counter...");
@@ -26,6 +28,68 @@ initial begin
         reset <= 0;
         wbus <= 0;
 
+        // initial bus - disconnected
+        #1
+        `assert(abus, 16'bZ);
+
+        // enable output from PC, value - undefined
+        outn <= 0;
+        #1
+        `assert(abus, 16'bX);
+
+        // reset
+        reset <= 1;
+        #1
+        `assert(abus, 16'b0);
+        reset <= 0;
+
+        // count up one
+        count <= 1;
+        #1
+        `tick(clk, 2);
+        `assert(abus, 16'b0); // should still be 0
+
+        `tick(iclk, 2);
+        `assert(abus, 16'b1); // load 1 into secondary stage
+
+        count <= 0;
+        outn <= 1;
+        #1
+
+        // parallel load
+        addr <= 16'hbeef;
+        wbus <= 1;
+        loadn <= 0;
+        #1
+        `tick(clk, 2);
+
+        wbus <= 0;
+        outn <= 0;
+        #1
+        `assert(abus, 16'b1); // should still be 1
+
+        `tick(iclk, 2);
+        `assert(abus, 16'hbeef); // load 0xbeef into secondary stage
+        loadn <= 1;
+
+        // reset
+        reset <= 1;
+        #1
+
+        // loop, check if increments correctly
+        reset <= 0;
+        count <= 1;
+        #1
+
+        for (i = 0; i < 'h10000 ; i = i + 1) begin
+            #1
+            `assert(abus, i);
+            `tick(clk, 2);
+            `tick(iclk, 2);
+        end
+
+        // should be wrapped around to 0
+        `assert(abus, 16'b0);
 
 end
 
