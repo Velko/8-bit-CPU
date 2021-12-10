544089f701e970aa534408d5abd78c863dcf8860
Jurģis Brigmanis <velko.x@gmail.com>
2021-12-10 19:24:42 +0200

    62256 memory chip

---
+++ emulator/chips/ram_62256.v
@@ -0,0 +1,29 @@
+module ram_62256 (
+    input [14:0] addr,
+    inout [7:0] data,
+
+    input csn,
+    input oen,
+    input wen
+);
+
+    reg [7:0] memory [0:32767];
+    reg [7:0] mem_out;
+
+    // Output/Tri-state
+    assign data = (!csn && !oen && wen) ? mem_out : 8'bz;
+
+    // Write
+    always @(addr or data or csn or wen) begin
+        if (!csn && !wen) begin
+            memory[addr] = data;
+        end
+    end
+
+    // Read
+    always @(addr or csn or wen) begin
+        if (!csn && wen) begin
+            mem_out = memory[addr];
+        end
+    end
+endmodule
