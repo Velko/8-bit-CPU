db2ca08e99386da0d201a658ed8ab4965d64e930
Jurģis Brigmanis <velko.x@gmail.com>
2021-12-28 20:46:18 +0200

    Run tests using Iverilog emulator

--- pycontrol/emu_check.sh
+++ pycontrol/emu_check.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-socat PTY,link=pty0,raw,echo=0 PTY,link=pty1,raw,echo=0 &
+socat PTY,link=pty0,raw,echo=0 PTY,link=../emulator/vm/pty1,raw,echo=0 &
 sleep 0.1
-../emulator/vm/cpu-server &
+make -C ../emulator/vm/ run &
 sleep 0.1
 SER_PORT=pty0 python3 -m pytest $@
 
