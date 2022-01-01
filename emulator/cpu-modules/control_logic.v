5c7359c647250993caac781c56cf7372074cbfcf
Jurģis Brigmanis <velko.x@gmail.com>
2022-01-01 17:51:43 +0200

    Implement extended opcodes

--- emulator/cpu-modules/control_logic.v
+++ emulator/cpu-modules/control_logic.v
@@ -14,10 +14,13 @@ module control_logic(
 
     reg [31:0] cword_out;
 
-    counter_161 cnt(.clk(iclk), .mrn(rstn), .pen(step_reset), .cep(1'b1), .d(4'b0), .cet(1'b1));
+    counter_161 cnt(.clk(iclk), .mrn(rstn), .pen(step_reset), .cep(!step_ext), .d(4'b0), .cet(1'b1));
 
-    always @(opcode or flags or cnt.q) begin
-        $read_control_rom(cword_out, cnt.q, flags, opcode);
+    //TODO: works, but step_ext signal "pulls the rug" from under itself
+    counter_161 ext(.clk(iclk), .mrn(rstn), .pen(step_reset), .cep(step_ext), .d(4'b0), .cet(1'b1));
+
+    always @(opcode or flags or cnt.q or ext.q) begin
+        $read_control_rom(cword_out, cnt.q, flags, {ext.q[0], opcode});
         //if (ctrlen == 1'b0)
         //    $display("x %d %b %h", cnt.q, flags, opcode);
     end
