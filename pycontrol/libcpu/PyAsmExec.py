bb2161db832efe1578e66b63eb272fdf69674136
Jurģis Brigmanis <velko.x@gmail.com>
2022-01-03 21:27:52 +0200

    Clean-up: remove obsolete CPU backends

--- pycontrol/libcpu/PyAsmExec.py
+++ pycontrol/libcpu/PyAsmExec.py
@@ -3,11 +3,9 @@ from .pinclient import PinClient
 from . import cpu
 from .ctrl_word import CtrlWord
 from .cpu_exec import CPUBackendControl
-from .cpu_assemble import CPUBackendAssemble
-from .cpu_emul import CPUBackendEmulate
 from .markers import InitializedBuffer
 from .test_helpers import CPUHelper
-from typing import Callable, Optional
+from typing import Optional
 
 control = CtrlWord()
 pins: Optional[PinClient] = None
@@ -26,16 +24,3 @@ def setup_data(s: InitializedBuffer) -> None:
     helper = CPUHelper(cpu.backend)
     helper.write_data(s)
 
-def compile(program: Callable[[], None]) -> None:
-    backend = CPUBackendAssemble()
-    cpu.install_cpu_backend(backend)
-    program()
-    backend.list()
-
-    binary = open("sieve.bin", "wb")
-    backend.bin(binary)
-    binary.close()
-
-def setup_emul() -> None:
-    backend = CPUBackendEmulate()
-    cpu.install_cpu_backend(backend)
