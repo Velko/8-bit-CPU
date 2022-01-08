9ad7bc603ce0bb4c46585fc7d471611973bb84b7
Jurģis Brigmanis <velko.x@gmail.com>
2022-01-08 20:05:49 +0200

    Verify contents

--- arduino/xmprog/client/xmclient.c
+++ arduino/xmprog/client/xmclient.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "cmd.h"
 #include "serial.h"
@@ -26,6 +27,76 @@ static void write_remote(FILE* port, struct cmd_options *cmd)
     pexec(fileno(port), read_params);
 }
 
+static void verify_data(FILE* port, struct cmd_options *cmd)
+{
+    FILE *local = fopen(cmd->file, "rb");
+    if (local == NULL)
+    {
+        perror("Local file");
+        exit(1);
+    }
+
+    char tmp_fname[20];
+
+    cmd->file = strcpy(tmp_fname, "tmpXXXXXX");
+    int tmp_fd = mkstemp(tmp_fname);
+    if (tmp_fd < 0)
+    {
+        perror("Temp file");
+        exit(1);
+    }
+
+    read_remote(port, cmd);
+
+    if (unlink(cmd->file) < 0)
+    {
+        perror("Unlink temp file");
+        exit(1);
+    }
+
+    FILE *remote = fdopen(tmp_fd, "rb");
+    if (remote == NULL)
+    {
+        perror("Remote file");
+        exit(1);
+    }
+
+    fprintf(stderr, "Comparing...");
+    fflush(stderr);
+
+    char lf_buf[1024];
+    char rf_buf[1024];
+
+    size_t position = 0;
+
+    for (;;)
+    {
+        size_t nloc = fread(lf_buf, 1, sizeof(lf_buf), local);
+        size_t nrem = fread(rf_buf, 1, sizeof(rf_buf), remote);
+
+        if (nloc != nrem)
+        {
+            fprintf(stderr, "File sizes differ at block offset %d\n", position);
+            exit(1);
+        }
+
+        if (memcmp(lf_buf, rf_buf, nloc) != 0)
+        {
+            fprintf(stderr, "File contents differ at block offset %d\n", position);
+            exit(1);
+        }
+
+        position += nloc;
+
+        if (nloc < sizeof(lf_buf)) break;
+    }
+
+    fclose(local);
+    fclose(remote);
+
+    fprintf(stderr, "Success!\n");
+}
+
 static FILE *initialize_writer(char *cmd_port)
 {
     int port_fd = serial_port_open(cmd_port);
@@ -35,7 +106,10 @@ static FILE *initialize_writer(char *cmd_port)
     FILE *port = fdopen(port_fd, "wb+");
 
     if (port == NULL)
+    {
         perror("Port");
+        exit(1);
+    }
 
     fprintf(stderr, "Waiting for device...");
     fflush(stderr);
@@ -67,6 +141,9 @@ int main(int argc, char **argv)
         break;
     case ACT_WRITE:
         write_remote(port, &cmd);
+        break;
+    case ACT_VERIFY:
+        verify_data(port, &cmd);
         break;
     }
 
