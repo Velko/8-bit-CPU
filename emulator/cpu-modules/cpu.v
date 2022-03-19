8f801facac13ea7b6324c3f3ea2ad3368d96edba
Jurģis Brigmanis <velko.x@gmail.com>
2022-03-19 13:07:52 +0200

    Report Out back to client

--- emulator/cpu-modules/cpu.v
+++ emulator/cpu-modules/cpu.v
@@ -12,6 +12,9 @@ module cpu(
 
         output brk,
         output hlt,
+        output reg [7:0] i_out,
+        output reg [7:0] c_out,
+        input out_rst,
 
         inout [31:0] control_word
     );
@@ -50,11 +53,20 @@ module cpu(
 
     //TODO: move to dedicated output modules
     always @(posedge clk) begin
-        if (load_mux_l.y[6] == 1'b0)
-            $display("%d", main_bus);
+        if (load_mux_l.y[6] == 1'b0) begin
+            //$display("%d", main_bus);
+            i_out <= main_bus;
+        end
 
-        if (load_mux_h.y[2] == 1'b0)
-            $write("%c", main_bus);
+        if (load_mux_h.y[2] == 1'b0) begin
+            //$write("%c", main_bus);
+            c_out <= main_bus;
+        end
+    end
+
+    always @(posedge out_rst) begin
+        i_out <= 8'bx;
+        c_out <= 8'bx;
     end
 
 endmodule
