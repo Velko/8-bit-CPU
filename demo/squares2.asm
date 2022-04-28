80c2d0e1dc7424b8e9e1f59eba4133e1f6c1a82c
Jurģis Brigmanis <velko.x@gmail.com>
2022-04-28 22:50:36 +0300

    Some more explanation for squares2

--- demo/squares2.asm
+++ demo/squares2.asm
@@ -13,6 +13,28 @@
 
 ;        print (A)
 
+;# there are small shortcomings:
+;# * A += B can wrap around briefly. It's still Ok, because the extra bit
+;#   ends up in Carry flag, and we can still recover that using ROR for the
+;"   subsequent shift;
+;# * there is no instruction to check the last bit of A, we have to do an AND
+;#   against another register (presumably D). It takes few instructions to
+;#   save/restore the D, and load 1 into it. And then we have to set up stack;
+;#
+;# So instead we rewrite it as follows:
+;
+;    for D in range(16):
+;        B = D
+;        A = B
+;        B <<= 3 # one less, because we're going to add AFTER the shift
+;        for C in range(4):
+;            A, carry = A >> 1, A & 1 != 0  # this is actually a single instruction: shr A
+;            if carry:                      # the last bit ended up in a Carry flag
+;                A += B                     # there's also no wrap-around issue
+;
+;        print (A)
+
+
 squares_start:
 
     clr D
