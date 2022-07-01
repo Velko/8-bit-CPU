2293e26066a31a8bcdedec580fc7b1ee24b21d2a
Jurģis Brigmanis <velko.x@gmail.com>
2022-07-01 22:50:48 +0300

    Switchable numeric display mode

--- emulator/cpu-modules/io_block.v
+++ emulator/cpu-modules/io_block.v
@@ -10,6 +10,9 @@ module io_block (
     input [3:0] loadctl
 );
 
+    wire signed [7:0] signed_bus;
+    reg [7:0] ifmt_mode;
+
     demux_138 load_mux_l(.e1n(splitter.loadctl[3]), .e2n(1'b0), .e3(1'b1), .a(splitter.loadctl[2:0]));
 
     io_control ctl(
@@ -32,14 +35,30 @@ module io_block (
         end
 
         if (ctl.sel_x[0] == 1'b0 && ctl.sel_y[0] == 1'b0) begin
-            $sformat(out_fmt, "#FOUT#%d\\n", main_bus);
+            case (ifmt_mode)
+            0: $sformat(out_fmt, "#FOUT# %d\\n", main_bus);
+            1: $sformat(out_fmt, "#FOUT#%d\\n", signed_bus);
+            2: $sformat(out_fmt, "#FOUT#h %h\\n", main_bus);
+            3: $sformat(out_fmt, "#FOUT#o%o\\n", main_bus);
+            default: $swrite(out_fmt, "#FOUT#x\\n");
+            endcase
         end
 
+        if (ctl.sel_x[1] == 1'b0 && ctl.sel_y[0] == 1'b0) begin
+            ifmt_mode <= main_bus;
+        end
     end
 
     always @(posedge out_rst or posedge reset) begin
         out_fmt <= 160'bx;
     end
 
+    always @(reset) begin
+        if (reset == 1'b1) begin
+            ifmt_mode <= 0;
+        end
+    end
+
+    assign signed_bus = main_bus;
 
 endmodule
