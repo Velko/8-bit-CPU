66bf21f825d863881acdc6c273260ec657f55410
Jurģis Brigmanis <velko.x@gmail.com>
2023-08-27 15:17:21 +0300

    Convert device definitions to dataclasses

--- pycontrol/libcpu/devices.py
+++ pycontrol/libcpu/devices.py
@@ -1,9 +1,11 @@
 from typing import Any
+from dataclasses import dataclass
 from .pin import Pin
 from .util import ControlSignal
+
+@dataclass
 class DeviceBase:
-    def __init__(self, name: str) -> None:
-        self.name = name
+    name: str
 
     def __setattr__(self, name: str, value: Any) -> None:
         if isinstance(value, ControlSignal):
@@ -13,38 +15,30 @@ class DeviceBase:
     def __str__(self) -> str:
         return self.name
 
-    def __repr__(self) -> str:
-        return self.name
-
+@dataclass
 class Register(DeviceBase):
-    def __init__(self, name: str, out: Pin, load: Pin) -> None:
-        DeviceBase.__init__(self, name)
-        self.out = out
-        self.load = load
+    out: Pin
+    load: Pin
 
+@dataclass
 class GPRegister(Register):
-    def __init__(self, name: str, out: Pin, load: Pin, alu_l: Pin, alu_r: Pin) -> None:
-        Register.__init__(self, name, out, load)
-        self.alu_l = alu_l
-        self.alu_r = alu_r
+    alu_l: Pin
+    alu_r: Pin
 
+@dataclass
 class WORegister(DeviceBase):
-    def __init__(self, name: str, load: Pin) -> None:
-        DeviceBase.__init__(self, name)
-        self.load = load
+    load: Pin
 
+@dataclass
 class ALU(DeviceBase):
-    def __init__(self, name: str, out: Pin, alt: Pin) -> None:
-        DeviceBase.__init__(self, name)
-        self.out = out
-        self.alt = alt
+    out: Pin
+    alt: Pin
 
 
+@dataclass
 class Flags(Register):
-    def __init__(self, name: str, out: Pin, load: Pin, calc: Pin, carry: Pin) -> None:
-        Register.__init__(self, name, out, load)
-        self.calc = calc
-        self.carry = carry
+    calc: Pin
+    carry: Pin
 
     V = 0b1000
     C = 0b0100
@@ -80,54 +74,44 @@ class Flags(Register):
 
         return f
 
+@dataclass
 class RAM(DeviceBase):
-    def __init__(self, name: str, out: Pin, write: Pin) -> None:
-        DeviceBase.__init__(self, name)
-        self.out = out
-        self.write = write
+    out: Pin
+    write: Pin
 
+@dataclass
 class Clock(DeviceBase):
-    def __init__(self, name: str, halt: Pin, brk: Pin) -> None:
-        DeviceBase.__init__(self, name)
-        self.halt = halt
-        self.brk  = brk
+    halt: Pin
+    brk: Pin
 
+@dataclass
 class StepCounter(DeviceBase):
-    def __init__(self, name: str, reset: Pin, extended: Pin) -> None:
-        DeviceBase.__init__(self, name)
-        self.reset = reset
-        self.extended = extended
+    reset: Pin
+    extended: Pin
 
+@dataclass
 class AddressRegister(Register):
-    def __init__(self, name: str, out: Pin, load: Pin) -> None:
-        Register.__init__(self, name, out, load)
+    pass
 
+@dataclass
 class ProgramCounter(AddressRegister):
-    def __init__(self, name: str, out: Pin, load: Pin, inc: Pin) -> None:
-        AddressRegister.__init__(self, name, out, load)
-        # To save on control lines, in current setup PC increment and output operations
-        # are merged, PC increments whenever it outputs and there's a clock tick.
-        # 2 aliased operations are defined to better describe intent in microcode.
-        self.inc = inc
+    inc: Pin
 
+@dataclass
 class StackPointer(AddressRegister):
-    def __init__(self, name: str, out: Pin, load: Pin, inc: Pin, dec: Pin) -> None:
-        AddressRegister.__init__(self, name, out, load)
-        self.inc = inc
-        self.dec = dec
+    inc: Pin
+    dec: Pin
 
+@dataclass
 class TransferRegister(Register):
-    def __init__(self, name: str, out: Pin, load: Pin) -> None:
-        Register.__init__(self, name, out, load)
+    pass
 
+@dataclass
 class AddressCalculator(Register):
-    def __init__(self, name: str, out: Pin, load: Pin, signed: Pin) -> None:
-        Register.__init__(self, name, out, load)
-        self.signed = signed
+    signed: Pin
 
+@dataclass
 class IOController(DeviceBase):
-    def __init__(self, name: str, laddr: Pin, from_dev: Pin, to_dev: Pin) -> None:
-        DeviceBase.__init__(self, name)
-        self.laddr = laddr
-        self.from_dev = from_dev
-        self.to_dev = to_dev
+    laddr: Pin
+    from_dev: Pin
+    to_dev: Pin
