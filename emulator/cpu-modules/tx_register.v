521da275e312b340d353b2712d2139168fe00094
Jurģis Brigmanis <velko.x@gmail.com>
2023-10-07 00:12:25 +0300

    Transfer Register using 377

--- emulator/cpu-modules/tx_register.v
+++ emulator/cpu-modules/tx_register.v
@@ -13,14 +13,14 @@ module tx_register (
 
 
     wire [7:0] out_v;
+    wire [7:0] sel_v;
 
     and_08p load_en(.a1(aloadn), .b1(mloadn), .a2(1'b0), .b2(1'b0), .a3(1'b0), .b3(1'b0), .a4(1'b0), .b4(1'b0));
 
-    mux_157b sel_in_l(.a(mbus[3:0]), .b(abus[3:0]), .en(1'b0), .sel(mloadn));
-    mux_157b sel_in_h(.a(mbus[7:4]), .b(abus[7:4]), .en(1'b0), .sel(mloadn));
+    mux_157b sel_in_l(.a(mbus[3:0]), .b(abus[3:0]), .en(1'b0), .sel(mloadn), .y(sel_v[3:0]));
+    mux_157b sel_in_h(.a(mbus[7:4]), .b(abus[7:4]), .en(1'b0), .sel(mloadn), .y(sel_v[7:4]));
 
-    dff_173 prim_l(.mr(reset), .cp(clk), .e1n(load_en.y1), .e2n(load_en.y1), .oe1n(1'b0), .oe2n(1'b0), .d(sel_in_l.y), .q(out_v[3:0]));
-    dff_173 prim_h(.mr(reset), .cp(clk), .e1n(load_en.y1), .e2n(load_en.y1), .oe1n(1'b0), .oe2n(1'b0), .d(sel_in_h.y), .q(out_v[7:4]));
+    dff_377 prim_reg(.cp(clk), .en(load_en.y1), .d(sel_v), .q(out_v));
 
     buffer_245 mbus_buf(.oen(moutn), .dir(1'b1), .a(out_v), .b(mbus));
     buffer_245 abus_buf(.oen(aoutn), .dir(1'b1), .a(out_v), .b(abus));
