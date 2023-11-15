ed69ae895d74e556e86efa1c67773658a0e997ea
Jurģis Brigmanis <velko.x@gmail.com>
2023-11-15 22:28:16 +0200

    Updated demo to use BIOS uart functions

--- demo/uart.asm
+++ demo/uart.asm
@@ -3,6 +3,19 @@
 ; To run the program, upload it to the CPU and reset it, but do not send "run" command.
 ; Then issue the command R from minicom session.
 
+    ; move cursor \x1B[13;35H
+    lea SDP, csi
+    call b_uart_puts
+
+    ldi A, 13
+    call b_uart_putdec
+    ldi A, ";"
+    out UART_DATA, A
+    ldi A, 35
+    call b_uart_putdec
+    ldi A, "H"
+    out UART_DATA, A
+
     ; print "message"
     lea SDP, message
     call b_uart_puts
@@ -33,5 +46,8 @@ repeat:
 exit:
     hlt
 
+csi:
+#d "\x1B[", 0x00
+
 message:
-#d "\x1B[13;35HHellorld!\r\n\0"
+#d "Hellorld!\r\n", 0x00
