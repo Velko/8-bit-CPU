63f4b57692af2b127f31ce0bcf807215a12a4530
Jurģis Brigmanis <velko.x@gmail.com>
2024-08-19 16:22:46 +0300

    Shorten startup

--- bios/bios.asm
+++ bios/bios.asm
@@ -19,10 +19,9 @@
 ; * load SP to dedicated stack area
 ; * jump to RAM location where the program should be loaded
 __start:
-    ldi A, 0x55 ; load something, because VerilogVM has X there by default and freaks out
-    clr A       ; when clr (essentially xor A, A) is tried. Should work fine on hardware.
-    mov B, A    ; Can not also ldi A, 0 directly, because we require bytes 0, 1, 2, 4 to
-    mov C, A    ; be non-zero for memory tests
+    ldi A, 0 ; load 0 explicitly, because VerilogVM has X there by default and freaks out
+    mov B, A ; when clr (essentially xor A, A) is tried. Should not been a problem on real hw.
+    mov C, A
     mov D, A
     lea SP, STACK_END
     jmp RAM_START
