e4b8823dddb51d815fe8a368b6f88af69d4fa0a6
Jurģis Brigmanis <velko.x@gmail.com>
2024-08-20 11:02:43 +0300

    Reformat SP, add tests

--- emulator/cpu-modules/stack_pointer.v
+++ emulator/cpu-modules/stack_pointer.v
@@ -11,23 +11,69 @@ module stack_pointer (
     inout [15:0] abus
 );
 
+    wire [15:0] addr;
+    wire p_loadn;
+    wire cpu;
+    wire cpd;
+
     // pl should follow !clk, when loadn == low
     // cpu should follow !clk, when cup == enabled (low)
 
     // pl = not(clk) or loadn
     // cpu = not(clk) or cupn
     // cpd = not(clk) or cdownn
-    not_04p inv(.a1(clk), .a2(1'b0), .a3(1'b0), .a4(1'b0), .a5(1'b0), .a6(1'b0));
-    or_32p ctrl(.a1(1'b0), .b1(1'b0), .a2(inv.y1), .b2(loadn), .a3(inv.y1), .b3(cupn), .a4(inv.y1), .b4(cdownn));
+    not_04p inv(
+        .a1(clk),
+         .a2(1'b0), .a3(1'b0), .a4(1'b0), .a5(1'b0), .a6(1'b0));
 
-    wire [15:0] addr;
+    or_32p ctrl(
+        .a1(1'b0), .b1(1'b0),
+        .a2(inv.y1), .b2(loadn), .y2(p_loadn),
+        .a3(inv.y1), .b3(cupn), .y3(cpu),
+        .a4(inv.y1), .b4(cdownn), .y4(cpd));
 
-    udcounter_193 cnt_0(.mr(reset), .pl(ctrl.y2), .d(abus[3:0]),   .q(addr[3:0]),   .cpu(ctrl.y3),   .cpd(ctrl.y4));
-    udcounter_193 cnt_1(.mr(reset), .pl(ctrl.y2), .d(abus[7:4]),   .q(addr[7:4]),   .cpu(cnt_0.tcu), .cpd(cnt_0.tcd));
-    udcounter_193 cnt_2(.mr(reset), .pl(ctrl.y2), .d(abus[11:8]),  .q(addr[11:8]),  .cpu(cnt_1.tcu), .cpd(cnt_1.tcd));
-    udcounter_193 cnt_3(.mr(reset), .pl(ctrl.y2), .d(abus[15:12]), .q(addr[15:12]), .cpu(cnt_2.tcu), .cpd(cnt_2.tcd));
+    udcounter_193 cnt_0(
+        .mr(reset),
+        .pl(p_loadn),
+        .d(abus[3:0]),
+        .q(addr[3:0]),
+        .cpu(cpu),
+        .cpd(cpd));
 
-    dff_374 outst_0(.cp(iclk), .oen(outn), .d(addr[7:0]), .q(abus[7:0]));
-    dff_374 outst_1(.cp(iclk), .oen(outn), .d(addr[15:8]), .q(abus[15:8]));
+    udcounter_193 cnt_1(
+        .mr(reset),
+        .pl(p_loadn),
+        .d(abus[7:4]),
+        .q(addr[7:4]),
+        .cpu(cnt_0.tcu),
+        .cpd(cnt_0.tcd));
+
+    udcounter_193 cnt_2(
+        .mr(reset),
+        .pl(p_loadn),
+        .d(abus[11:8]),
+        .q(addr[11:8]),
+        .cpu(cnt_1.tcu),
+        .cpd(cnt_1.tcd));
+
+    udcounter_193 cnt_3(
+        .mr(reset),
+        .pl(p_loadn),
+        .d(abus[15:12]),
+        .q(addr[15:12]),
+        .cpu(cnt_2.tcu),
+        .cpd(cnt_2.tcd));
+
+    dff_374 outst_0(
+        .cp(iclk),
+        .oen(outn),
+        .d(addr[7:0]),
+        .q(abus[7:0]));
+
+    dff_374 outst_1(
+        .cp(iclk),
+        .oen(outn),
+        .d(addr[15:8]),
+        .q(abus[15:8]));
 
 endmodule
---
+++ emulator/tests/tb_stack_pointer.v
@@ -0,0 +1,141 @@
+module tb_stack_pointer;
+
+    reg outn;
+    reg loadn;
+    reg cupn;
+    reg cdownn;
+
+    reg clk;
+    reg iclk;
+    reg reset;
+
+    wire [15:0] abus;
+    reg [15:0] addr;
+    reg wbus;
+
+    integer i;
+
+    stack_pointer sp(
+        .outn(outn),
+        .loadn(loadn),
+        .cupn(cupn),
+        .cdownn(cdownn),
+        .clk(clk),
+        .iclk(iclk),
+        .reset(reset),
+        .abus(abus));
+
+initial begin
+        $display("Stack pointer...");
+
+        outn <= 1;
+        loadn <= 1;
+        cupn <= 1;
+        cdownn <= 1;
+        clk <= 0;
+        iclk <= 0;
+        reset <= 0;
+        wbus <= 0;
+
+        // initial bus - disconnected
+        #1
+        `assert(abus, 16'bZ);
+
+        // enable output from PC, value - undefined
+        outn <= 0;
+        #1
+        `assert(abus, 16'bX);
+
+        // reset
+        reset <= 1;
+        #1
+        `assert(abus, 16'bX);
+
+        `tick(iclk, 2); // reset value propagation requires ICLK tick
+        `assert(abus, 16'b0);
+
+
+        reset <= 0;
+
+        // count up one
+        cupn <= 0;
+        #1
+        `tick(clk, 2);
+        `assert(abus, 16'b0); // should still be 0
+
+        `tick(iclk, 2);
+        `assert(abus, 16'b1); // load 1 into secondary stage
+
+        cupn <= 1;
+        #1
+
+        // count down one
+        cdownn <= 0;
+        #1
+        `tick(clk, 2);
+        `assert(abus, 16'b1); // should still be 1
+
+        `tick(iclk, 2);
+        `assert(abus, 16'b0); // load 0 into secondary stage
+
+        cdownn <= 1;
+        outn <= 1;
+        #1
+
+        // parallel load
+        addr <= 16'hbeef;
+        wbus <= 1;
+        loadn <= 0;
+        #1
+        `tick(clk, 2);
+
+        wbus <= 0;
+        outn <= 0;
+        #1
+        `assert(abus, 16'b0); // should still be 0
+
+        `tick(iclk, 2);
+        `assert(abus, 16'hbeef); // load 0xbeef into secondary stage
+        loadn <= 1;
+
+        // reset
+        reset <= 1;
+        #1
+
+        // loop, check if increments correctly
+        reset <= 0;
+        `tick(iclk, 2);
+        cupn <= 0;
+        #1
+
+        for (i = 0; i < 'h10000 ; i = i + 1) begin
+            #1
+            `assert(abus, i);
+            `tick(clk, 2);
+            `tick(iclk, 2);
+        end
+        cupn <= 1;
+
+        // should be wrapped around to 0
+        `assert(abus, 16'b0);
+
+        // loop, check if decrements correctly
+        cdownn <= 0;
+        #1
+
+        for (i = 'h10000; i >= 0  ; i = i - 1) begin
+            #1
+            `assert(abus, (i & 'hffff));
+            `tick(clk, 2);
+            `tick(iclk, 2);
+        end
+        cdownn <= 1;
+
+        // should be wrapped around to 0xFFFF
+        `assert(abus, 16'hffff);
+
+end
+
+    assign abus = wbus ? addr : 16'bZ;
+
+endmodule
