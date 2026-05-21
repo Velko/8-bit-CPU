d50f8bea292d0e655901b15ca275ab9114ad68e7
Jurģis Brigmanis <velko.x@gmail.com>
2026-05-21 17:46:02 +0300

    Modulo-38 C reference code

--- demo/rand8.c
+++ demo/rand8.c
@@ -28,6 +28,22 @@ void init_rng(uint8_t s1, uint8_t s2, uint8_t s3) {
   rnd8();
 }
 
+uint8_t nibble_swap(uint8_t x) {
+    return (x << 4) | (x >> 4);
+}
+
+uint8_t reduce_to_38_fast(uint8_t random_8bit) {
+    uint8_t swap = nibble_swap(random_8bit);
+    uint8_t x_gt_4 = swap & 0x0F;
+
+    // Calculate pieces using minimal steps
+    uint8_t x_gt_6 = x_gt_4 >> 2;
+    uint8_t x_gt_7 = x_gt_6 >> 1; // Just 1 extra shift!
+    uint8_t x_gt_3 = (x_gt_4 << 1) | (swap >> 7);
+
+    return x_gt_3 + x_gt_6 + x_gt_7;
+}
+
 int main() {
     /* Initialize with some arbitrary values */
     init_rng(0x01, 0x02, 0x03);
@@ -35,7 +51,8 @@ int main() {
     /* Generate and print some random numbers */
     for (int i = 0; i < 256; i++) {
         uint8_t r = rnd8();
-        printf("\033[1;31mh %02x\n\033[0m", r);
+        uint8_t reduced = reduce_to_38_fast(r);
+        printf("\033[1;31mh %02x\n\033[0m", reduced);
     }
 
     return 0;
