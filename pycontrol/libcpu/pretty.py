2d870b9f7ddba98b5ff33529aba33b3400472686
Jurģis Brigmanis <velko.x@gmail.com>
2026-05-28 14:55:59 +0300

    Color LCD output

--- pycontrol/libcpu/pretty.py
+++ pycontrol/libcpu/pretty.py
@@ -2,10 +2,14 @@
 def ansi_red(text: str) -> str:
     return f"\x1b[1;31m{text}\x1b[0m"
 
+def ansi_green(text: str) -> str:
+    return f"\x1b[1;32m{text}\x1b[0m"
+
 def format_message(target: int, msg: str) -> str:
     match target:
         case 0:
             return ansi_red(msg)
+        case 0x10:
+            return ansi_green(msg)
         case _:
             return msg
-    return msg
