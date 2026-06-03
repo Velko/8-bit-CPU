d604b156c6cf69c550971a3f197c8fd58f26fd94
Jurģis Brigmanis <velko.x@gmail.com>
2026-06-03 10:45:36 +0300

    Color changed regs

--- pycontrol/tdbg/libtdb/regs.py
+++ pycontrol/tdbg/libtdb/regs.py
@@ -7,12 +7,20 @@ class RegistersView(Static):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
         self.table = Table("Register", "Value")
+        self.old_values: dict[str, str] = {}
 
     def on_mount(self) -> None:
         self.update(self.table)
 
     def update_table(self, registers: dict[str, str]) -> None:
+        if len(self.old_values) == 0:
+            self.old_values = registers
         self.table = Table("Register", "Value")
         for reg, val in registers.items():
-            self.table.add_row(reg, val)
+            old_val = self.old_values.get(reg)
+            if old_val is not None and old_val != val:
+                self.table.add_row(reg, f"[bold dark_red]{val}[/bold dark_red]")
+            else:
+                self.table.add_row(reg, val)
+            self.old_values[reg] = val
         self.update(self.table)
