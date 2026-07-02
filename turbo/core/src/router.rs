2f6cfa8e475284fdee0b1f12643d2fa26de72837
Jurģis Brigmanis <velko.x@gmail.com>
2026-07-02 17:06:52 +0300

    Test turning Off

--- turbo/core/src/router.rs
+++ turbo/core/src/router.rs
@@ -21,16 +21,19 @@ mod tests {
     #[test]
     fn test_static_dispatch() {
         let device_map = DeviceMap::new();
-        let old_cw = 0x07ff58ff; // default
-        let new_cw = 0x07ff0915; // add_B_C
+        let default_cw = 0x07ff58ff; // default
+        let add_bc_cw = 0x07ff0915; // add_B_C
         println!("add_B_C");
-        device_map.route_word(old_cw, new_cw);
+        device_map.route_word(default_cw, add_bc_cw);
 
-        let newer_cw = 0x07ff9805; // inc A
+        let inc_a_cw = 0x07ff9805; // inc A
         println!("inc_A");
-        device_map.route_word(new_cw, newer_cw);
+        device_map.route_word(add_bc_cw, inc_a_cw);
 
 
-        assert_eq!(old_cw, new_cw); // always fails, just to demonstrate the test
+        println!("Off");
+        device_map.route_word(inc_a_cw, default_cw);
+
+        assert_eq!(default_cw, inc_a_cw); // always fails, just to demonstrate the test
     }
 }
