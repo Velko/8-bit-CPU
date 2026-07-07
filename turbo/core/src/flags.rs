45069b6e8eadc3f1f771cbe632f6c6cfb3dc759b
Jurģis Brigmanis <velko.x@gmail.com>
2026-07-07 18:03:06 +0300

    Extract flags

---
+++ turbo/core/src/flags.rs
@@ -0,0 +1,116 @@
+use std::cell::Cell;
+use std::ops::{BitOr, BitAnd, BitOrAssign};
+use std::fmt::Debug;
+use crate::devices::Buses;
+use crate::devices::MainBusValue;
+use crate::devices::OutReceiver;
+use crate::devices::LoadReceiver;
+use crate::devices::ClockReceiver;
+use crate::router::DeviceMap;
+
+
+
+use crate::devices::Peek;
+
+#[derive(Clone, Copy, PartialEq, Default)]
+pub struct Flags {
+    value: u8,
+}
+
+impl Flags {
+    pub const Empty: Flags = Flags { value: 0b0000 };
+    pub const V: Flags = Flags { value: 0b1000 };
+    pub const C: Flags = Flags { value: 0b0100 };
+    pub const Z: Flags = Flags { value: 0b0010 };
+    pub const N: Flags = Flags { value: 0b0001 };
+}
+
+impl Debug for Flags {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut flags = String::with_capacity(4);
+        flags.push(if *self & Self::V != Self::Empty { 'V' } else { '-' });
+        flags.push(if *self & Self::C != Self::Empty { 'C' } else { '-' });
+        flags.push(if *self & Self::Z != Self::Empty { 'Z' } else { '-' });
+        flags.push(if *self & Self::N != Self::Empty { 'N' } else { '-' });
+        write!(f, "Flags({})", flags)
+    }
+}
+
+impl BitOr for Flags {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Flags { value: self.value | rhs.value }
+    }
+}
+
+impl BitAnd for Flags {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Flags { value: self.value & rhs.value }
+    }
+}
+
+impl BitOrAssign for Flags {
+    fn bitor_assign(&mut self, rhs: Self) {
+        self.value |= rhs.value;
+    }
+}
+
+
+pub struct FlagsRegister {
+    pub name: &'static str,
+    value_primary: Flags,
+    value_secondary: Flags,
+    calc_enabled: Cell<bool>,
+}
+impl OutReceiver for FlagsRegister {}
+impl LoadReceiver for FlagsRegister {}
+impl FlagsRegister {
+    pub fn new(name: &'static str) -> Self {
+        Self {
+            name,
+            value_primary: Flags::Empty,
+            value_secondary: Flags::Empty,
+            calc_enabled: Cell::new(false)
+        }
+    }
+    pub fn on_calc_change(&self, _buses: &mut Buses, new_state: bool) {
+        println!("FlagsRegister {} Calc changed to: {}", self.name, new_state);
+        self.calc_enabled.set(new_state);
+    }
+    pub fn on_carry_change(&self, buses: &mut Buses, new_state: bool) {
+        println!("FlagsRegister {} Carry changed to: {}", self.name, new_state);
+        buses.carry_in = new_state;
+    }
+}
+impl ClockReceiver for FlagsRegister {
+    fn on_clock_tick_primary(&mut self, buses: &mut Buses) {
+        if self.calc_enabled.get() {
+            // Perform Z and N calculations based on the main bus value
+            let result = buses.resolve_main_bus();
+            let mut new_value = Flags::Empty;
+            if result == 0 {
+                new_value |= Flags::Z;
+            }
+            if (result & 0x80) != 0 {
+                new_value |= Flags::N;
+            }
+
+            let (carry, overflow) = buses.resolve_alu_flags();
+            new_value |= carry.unwrap_or(self.value_primary & Flags::C); // Apply new or preserve previous carry if not calculated
+            new_value |= overflow.unwrap_or(self.value_primary & Flags::V); // Apply new or preserve previous overflow if not calculated
+            self.value_primary = new_value;
+        }
+    }
+    fn on_clock_tick_secondary(&mut self, _buses: &mut Buses) {
+        self.value_secondary = self.value_primary;
+    }
+}
+
+impl Peek<Flags> for FlagsRegister {
+    fn peek(&self) -> Flags {
+        self.value_secondary
+    }
+}
--- turbo/core/src/lib.rs
+++ turbo/core/src/lib.rs
@@ -2,5 +2,6 @@ mod router;
 mod devices;
 mod gp_register;
 mod alu;
+mod flags;
 
 type ControlWord = u32;
