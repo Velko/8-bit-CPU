c4d6f2b9bcd2d9ed74cb5e9865fe2c00661ad966
Jurģis Brigmanis <velko.x@gmail.com>
2026-07-31 16:30:44 +0300

    Implement address calc

--- turbo/core/src/address_calculator.rs
+++ turbo/core/src/address_calculator.rs
@@ -5,6 +5,7 @@ pub struct AddressCalculator {
     pub out: BusOutputPin<AddressBusSource>,
     pub load: DelayedPin,
     pub signed: DelayedPin,
+    value: u16,
 }
 
 impl AddressCalculator {
@@ -13,13 +14,74 @@ impl AddressCalculator {
             name,
             out: BusOutputPin::new(address_bus_id),
             load: DelayedPin::new(),
-            signed: DelayedPin::new()
+            signed: DelayedPin::new(),
+            value: 0,
         }
     }
 }
-impl GlobalSignalsReceiver for AddressCalculator {}
+
+impl GlobalSignalsReceiver for AddressCalculator {
+    fn on_clock_tick_primary(&mut self, bus_values: &BusValues) {
+        if self.load.is_enabled() {
+            let base_address = bus_values.address_bus.value.unwrap();
+            self.value = if self.signed.is_enabled() {
+                let signed_offset = bus_values.main_bus.value.unwrap() as i8 as i16;
+                base_address.wrapping_add_signed(signed_offset)
+            } else {
+                base_address.wrapping_add(bus_values.main_bus.value.unwrap() as u16)
+            }
+        }
+    }
+
+    fn on_reset(&mut self) {
+        self.value = 0;
+    }
+}
+
 impl ValueSource<u16> for AddressCalculator {
     fn get_value(&self, _bus_values: &BusValues) -> u16 {
-        todo!()
+        self.value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{DEFAULT_CW, control_word::ControlWordBuilder, router::AddrLoadMux, test_helpers::TestBench, router::ACalcSigned};
+
+    #[test]
+    fn test_load_unsigned() {
+        let mut bench = TestBench::new();
+
+        let load_calc_unsigned = ControlWordBuilder::default()
+            .apply_mux::<AddrLoadMux>(AddrLoadMux::VALUE_ACALC_LOAD)
+            .build();
+
+        bench.devices.route_word(&mut bench.bus_values, DEFAULT_CW, load_calc_unsigned);
+        bench.bus_values.main_bus.value = Some(0x20);
+        bench.bus_values.address_bus.value = Some(0x1000);
+
+        bench.devices.broadcast_clock_tick_primary(&bench.bus_values);
+
+
+        assert_eq!(bench.devices.ACalc.value, 0x1020);
+    }
+
+    #[test]
+    fn test_load_signed() {
+        let mut bench = TestBench::new();
+
+        let load_calc_unsigned = ControlWordBuilder::default()
+            .apply_mux::<AddrLoadMux>(AddrLoadMux::VALUE_ACALC_LOAD)
+            .apply_bit::<ACalcSigned>()
+            .build();
+
+        bench.devices.route_word(&mut bench.bus_values, DEFAULT_CW, load_calc_unsigned);
+        bench.bus_values.main_bus.value = Some(0x80);
+        bench.bus_values.address_bus.value = Some(0x1000);
+
+        bench.devices.broadcast_clock_tick_primary(&bench.bus_values);
+
+
+        assert_eq!(bench.devices.ACalc.value, 0x0F80);
     }
 }
