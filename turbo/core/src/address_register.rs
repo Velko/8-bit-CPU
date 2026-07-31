aa28ce240e22a1db2f6ad4ef3f81485e79c4a85e
Jurģis Brigmanis <velko.x@gmail.com>
2026-07-31 15:48:27 +0300

    Implement Address Register

--- turbo/core/src/address_register.rs
+++ turbo/core/src/address_register.rs
@@ -4,22 +4,33 @@ pub struct AddressRegister {
     pub name: &'static str,
     pub out: BusOutputPin<AddressBusSource>,
     pub load: DelayedPin,
+    value: u16,
 }
 impl AddressRegister {
     pub fn new(name: &'static str, address_bus_id: AddressBusSource) -> Self {
         Self {
             name,
             out: BusOutputPin::new(address_bus_id),
-            load: DelayedPin::new()
+            load: DelayedPin::new(),
+            value: 0,
         }
     }
 }
 
 impl ValueSource<u16> for AddressRegister {
     fn get_value(&self, _bus_values: &BusValues) -> u16 {
-        todo!()
+        self.value
     }
 }
 
-
-impl GlobalSignalsReceiver for AddressRegister {}
+impl GlobalSignalsReceiver for AddressRegister {
+    fn on_clock_tick_primary(&mut self, bus_values: &BusValues) {
+        if self.load.is_enabled() {
+            let value = bus_values.address_bus.value.unwrap();
+            self.value = value;
+        }
+    }
+    fn on_reset(&mut self) {
+        self.value = 0;
+    }
+}
