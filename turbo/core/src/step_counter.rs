beafb0f99b641d7adf49ccc54e84c5632547ee65
Jurģis Brigmanis <velko.x@gmail.com>
2026-08-05 11:40:01 +0300

    Step counter

--- turbo/core/src/step_counter.rs
+++ turbo/core/src/step_counter.rs
@@ -1,14 +1,93 @@
-use crate::{BusValues, devices::GlobalSignalsReceiver};
+use crate::{BusValues, devices::{DelayedPin, GlobalSignalsReceiver, ValueSource}};
 
 pub struct StepCounter {
     pub name: &'static str,
+    pub reset: DelayedPin,
+    pub extended: DelayedPin, //TODO: does this pin really belongs here?
+    step: u8,
+    extval: usize,
 }
 
 impl StepCounter {
     pub fn new(name: &'static str) -> Self {
-        Self { name }
+        Self {
+            name,
+            reset: DelayedPin::new(),
+            extended: DelayedPin::new(),
+            step: 0,
+            extval: 0,
+        }
     }
-    pub fn on_reset_change(&self, _bus_values: &mut BusValues, _enable: bool) {}
-    pub fn on_extended_change(&self, _bus_values: &mut BusValues, _enable: bool) {}
+
+    pub fn get_extended_value(&self) -> usize {
+        self.extval
+    }
 }
-impl GlobalSignalsReceiver for StepCounter {}
+
+impl GlobalSignalsReceiver for StepCounter {
+    fn on_clock_tick_primary(&mut self, _bus_values: &mut BusValues) {
+        if self.extended.is_enabled() {
+            // while hardware only allows an extra bit, we can future-proof and support more
+            self.extval += 0x1;
+        }
+    }
+
+    fn on_clock_tick_secondary(&mut self) {
+        if self.reset.is_enabled() {
+            self.step = 0;
+            self.extval = 0;
+        } else {
+            self.step += 1;
+        }
+    }
+
+    fn on_reset(&mut self) {
+        self.step = 0;
+        self.extval = 0;
+    }
+}
+
+impl ValueSource<u8> for StepCounter {
+    fn get_value(&self, _bus_values: &BusValues) -> u8 {
+        self.step
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{devices::ValueSource, test_helpers::TestBench};
+
+
+    #[test]
+    fn test_step_counter_increment() {
+        let mut bench = TestBench::new();
+
+        bench.devices.broadcast_clock_tick_secondary();
+        assert_eq!(bench.devices.StepCounter.get_value(&bench.bus_values), 1);
+
+
+        bench.devices.broadcast_clock_tick_secondary();
+        assert_eq!(bench.devices.StepCounter.get_value(&bench.bus_values), 2);
+    }
+
+    #[test]
+    fn test_step_counter_reset() {
+        let mut bench = TestBench::new();
+        bench.devices.StepCounter.step = 5;
+
+        bench.devices.StepCounter.reset.change(&bench.devices.StepCounter, &mut bench.bus_values, true);
+        bench.devices.broadcast_clock_tick_secondary();
+
+        assert_eq!(bench.devices.StepCounter.get_value(&bench.bus_values), 0);
+    }
+
+    #[test]
+    fn test_step_counter_extended() {
+        let mut bench = TestBench::new();
+
+        bench.devices.StepCounter.extended.change(&bench.devices.StepCounter, &mut bench.bus_values, true);
+        bench.devices.broadcast_clock_tick_primary(&mut bench.bus_values);
+
+        assert_eq!(bench.devices.StepCounter.get_extended_value(), 0x1);
+    }
+}
