ccbf77f7ac0469e1a784c1ea401d44a089ec89c8
Jurģis Brigmanis <velko.x@gmail.com>
2026-08-07 16:14:00 +0300

    Extract peripherals

--- turbo/core/src/io_controller.rs
+++ turbo/core/src/io_controller.rs
@@ -1,4 +1,4 @@
-use crate::{BusValues, IOMessage, devices::{BusOutputPin, DelayedPin, GlobalSignalsReceiver, ValueSource}, display_lcd::Lcd, router::MainBusSource, uart::Uart};
+use crate::{BusValues, devices::{BusOutputPin, DelayedPin, GlobalSignalsReceiver, ValueSource}, peripherals::Peripherals, router::MainBusSource};
 
 pub struct IOController {
     pub name: &'static str,
@@ -6,10 +6,7 @@ pub struct IOController {
     pub to_dev: DelayedPin,
     pub laddr: DelayedPin,
     selected_port: u8,
-    display_numeric: DisplayNumeric,
-    display_char: DisplayChar,
-    lcd: Lcd,
-    uart: Uart,
+    peripherals: Peripherals,
 }
 
 impl IOController {
@@ -20,26 +17,14 @@ impl IOController {
             to_dev: DelayedPin::new(),
             laddr: DelayedPin::new(),
             selected_port: 0,
-            display_numeric: DisplayNumeric::new(),
-            display_char: DisplayChar::new(),
-            lcd: Lcd::new(),
-            uart: Uart::new(),
+            peripherals: Peripherals::new(),
         }
-    }
-
-    fn wrap_message(&self, payload: String) -> Option<IOMessage> {
-        Some(IOMessage::Out { payload, port: self.selected_port })
     }
 }
 
 impl ValueSource<u8> for IOController {
     fn get_value(&self, _bus_values: &BusValues) -> u8 {
-        match self.selected_port {
-            0x11 => self.lcd.get_status(),
-            0x20 => self.uart.get_status(),
-            0x21 => self.uart.get_char(),
-            _ => todo!("Port: 0x{:02x} input not yet implemented", self.selected_port),
-        }
+        self.peripherals.read_port(self.selected_port)
     }
 }
 
@@ -48,67 +33,7 @@ impl GlobalSignalsReceiver for IOController {
         if self.laddr.is_enabled() {
             self.selected_port = bus_values.main_bus.value.unwrap();
         } else if self.to_dev.is_enabled() {
-            match self.selected_port {
-                0 => {
-                    bus_values.message = self.wrap_message(self.display_numeric.format(bus_values.main_bus.value.unwrap()));
-                },
-                1 => {
-                    self.display_numeric.set_mode(bus_values.main_bus.value.unwrap());
-                },
-                4 => {
-                    bus_values.message = self.wrap_message(self.display_char.format(bus_values.main_bus.value.unwrap()));
-                },
-                0x10 => {
-                    if let Some(msg) = self.lcd.send_data(bus_values.main_bus.value.unwrap()) {
-                        bus_values.message = self.wrap_message(msg);
-                    }
-                },
-                0x11 => {
-                    self.lcd.send_command(bus_values.main_bus.value.unwrap());
-                },
-                0x21 => {
-                    self.uart.send_char(bus_values.main_bus.value.unwrap());
-                },
-                _ => todo!("Port: 0x{:02x} not yet implemented", self.selected_port),
-            };
+            bus_values.message = self.peripherals.write_port(self.selected_port, bus_values.main_bus.value.unwrap());
         }
     }
 }
-
-struct DisplayNumeric {
-    mode: u8,
-}
-
-impl DisplayNumeric {
-    pub fn new() -> Self {
-        Self { mode: 0 }
-    }
-
-    pub fn format(&self, value: u8) -> String {
-        match self.mode {
-            0 => format!("{:4}\\n", value),
-            1 => format!("{:4}\\n", value as i8),
-            2 => format!("h {:02x}\\n", value),
-            3 => format!("o{:03o}\\n", value),
-            _ => panic!("DisplayNumeric: unsupported mode {}", self.mode),
-        }
-    }
-
-    pub fn set_mode(&mut self, mode: u8) {
-        self.mode = mode;
-    }
-}
-
-struct DisplayChar {
-}
-
-impl DisplayChar {
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    pub fn format(&self, value: u8) -> String {
-        format!("{}", value as char)
-    }
-}
-
---
+++ turbo/core/src/peripherals.rs
@@ -0,0 +1,93 @@
+use crate::{IOMessage, display_lcd::Lcd, uart::Uart};
+
+pub struct Peripherals {
+    display_numeric: DisplayNumeric,
+    display_char: DisplayChar,
+    lcd: Lcd,
+    uart: Uart,
+}
+
+impl Peripherals {
+    pub fn new() -> Self {
+        Self {
+            display_numeric: DisplayNumeric::new(),
+            display_char: DisplayChar::new(),
+            lcd: Lcd::new(),
+            uart: Uart::new(),
+        }
+    }
+
+    pub fn read_port(&self, port: u8) -> u8 {
+        match port {
+            0x11 => self.lcd.get_status(),
+            0x20 => self.uart.get_status(),
+            0x21 => self.uart.get_char(),
+            _ => todo!("Port: 0x{:02x} input not yet implemented", port),
+        }
+    }
+
+    pub fn write_port(&mut self, port: u8, value: u8) -> Option<IOMessage> {
+        match port {
+            0 => {
+                return Some(IOMessage::Out { payload: self.display_numeric.format(value), port: port });
+            },
+            1 => {
+                self.display_numeric.set_mode(value);
+            },
+            4 => {
+                return Some(IOMessage::Out { payload: self.display_char.format(value), port: port });
+            },
+            0x10 => {
+                if let Some(msg) = self.lcd.send_data(value) {
+                    return Some(IOMessage::Out { payload: msg, port: port });
+                }
+            },
+            0x11 => {
+                self.lcd.send_command(value);
+            },
+            0x21 => {
+                self.uart.send_char(value);
+            },
+            _ => todo!("Port: 0x{:02x} not yet implemented", port),
+        }
+        None
+    }
+}
+
+struct DisplayNumeric {
+    mode: u8,
+}
+
+impl DisplayNumeric {
+    pub fn new() -> Self {
+        Self { mode: 0 }
+    }
+
+    pub fn format(&self, value: u8) -> String {
+        match self.mode {
+            0 => format!("{:4}\\n", value),
+            1 => format!("{:4}\\n", value as i8),
+            2 => format!("h {:02x}\\n", value),
+            3 => format!("o{:03o}\\n", value),
+            _ => panic!("DisplayNumeric: unsupported mode {}", self.mode),
+        }
+    }
+
+    pub fn set_mode(&mut self, mode: u8) {
+        self.mode = mode;
+    }
+}
+
+struct DisplayChar {
+}
+
+impl DisplayChar {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn format(&self, value: u8) -> String {
+        format!("{}", value as char)
+    }
+}
+
