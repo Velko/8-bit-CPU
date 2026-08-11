f83e4ccfe0a1bfc83bc799abe1f1ba1c92e9e894
Jurģis Brigmanis <velko.x@gmail.com>
2026-08-11 16:29:57 +0300

    Upgrade test-all script

--- tools/test_all.sh
+++ tools/test_all.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
 set -e
 
+
+run_emulator_tests() {
+    python3 -m pytest
+
+    cd ../demo
+    ../tools/exec_bin.py hello.bin | grep -q "Hello, World!" || echo "FAILED: hello.bin"
+    ../pycontrol/tools/debugger.py -s hello.bin | grep -q "Hello, World!" || echo "FAILED: steprun hello.bin"
+    ../tools/exec_bin.py prime_sieve.bin | diff -u primes.txt - || echo "FAILED: prime_sieve.bin"
+    ../tools/exec_bin.py double_dabble.bin | grep -q "36324058" || echo "FAILED: double_dabble.bin"
+    ../tools/exec_bin.py -M uart_output.bin | diff -u uart_output.txt - || echo "FAILED: uart_output.bin"
+    cd -
+
+    ../tools/shutdown.py
+}
+
+echo "********************** Verilog Emulator Tests **********************"
 cd $(dirname $0)/../emulator/tests
 make clean
 make
 
-cd ../../pycontrol
+echo "********************** Rust Emulator Tests **********************"
+cd ../../turbo
+cargo test
+
+echo "********************** PyControl TypeCheck **********************"
+cd ../pycontrol
 ./mypy_check.sh
 
+echo "********************** PyControl Verilog Emulator Tests **********************"
 ../tools/start_emu.sh
-python3 -m pytest
+run_emulator_tests
 
-
-cd ../demo
-
-../tools/exec_bin.py hello.bin | grep -q "Hello, World!" || echo "FAILED: hello.bin"
-../pycontrol/tools/debugger.py -s hello.bin | grep -q "Hello, World!" || echo "FAILED: steprun hello.bin"
-../tools/exec_bin.py prime_sieve.bin | diff -u primes.txt - || echo "FAILED: prime_sieve.bin"
-../tools/exec_bin.py double_dabble.bin | grep -q "36324058" || echo "FAILED: double_dabble.bin"
-../tools/exec_bin.py -M uart_output.bin | diff -u uart_output.txt - || echo "FAILED: uart_output.bin"
-
-
-../tools/shutdown.py
+echo "********************** PyControl Rust Emulator Tests **********************"
+../tools/start_turbo.sh
+run_emulator_tests
