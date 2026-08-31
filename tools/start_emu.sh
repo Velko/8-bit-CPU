e6ba71aeb7b4f41f7dbe9c36c656d124ed4a92ca
Jurģis Brigmanis <velko.x@gmail.com>
2026-08-31 17:11:09 +0300

    Separate build and run

--- tools/start_emu.sh
+++ tools/start_emu.sh
@@ -5,5 +5,6 @@ killall -9 vvp turbo-vm || true
 VMPATH=$(dirname $0)/../emulator/vm
 
 #socat /dev/ttyUSB0,raw,echo=0 udp-listen:8888,reuseaddr
+make -C $VMPATH all
 make -C $VMPATH run &
 sleep 0.1
--- tools/start_turbo.sh
+++ tools/start_turbo.sh
@@ -4,5 +4,6 @@ killall -9 vvp turbo-vm || true
 
 VMPATH=$(dirname $0)/../turbo
 cd $VMPATH
+cargo build
 cargo run &
 sleep 0.1
